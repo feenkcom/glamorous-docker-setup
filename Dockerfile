f837af3d87b1dd91d753c068de68e33b51322eca
George Ganea <georgeganea@gmail.com>
2021-01-31 16:21:50 +0200

    load the "server" code on top of the latest Glamorous Toolkit image

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM ubuntu:latest
 RUN apt update && apt install -y curl unzip libgit2-dev
 ADD dockerimage.sh load.st run.st ./
 RUN --mount=type=secret,id=myid_rsa --mount=type=secret,id=myid_rsa.pub /bin/sh dockerimage.sh
-CMD ["./glamoroustoolkit", "Pharo.image", "st", "run.st"]
+CMD ["./GlamorousToolkit/glamoroustoolkit", "./GlamorousToolkit/GlamorousToolkit.image", "st", "run.st"]
--- dockerimage.sh
+++ dockerimage.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
-#get the latest glamorous toolkit vm
-curl -L https://github.com/feenkcom/opensmalltalk-vm/releases/latest/download/build-artifacts.zip -o build-artifacts.zip
-unzip build-artifacts.zip
-unzip build-artifacts/GlamorousToolkitVM-*-linux64-bin.zip
-#get the latest pharo image
-curl -L https://get.pharo.org/64/80 | bash
 
+#get the latest gt image
+curl -L https://github.com/feenkcom/gtoolkit/releases/latest/download/GT.zip -o GT.zip
+unzip GT.zip
+
+#get the latest glamorous toolkit vm
+cd GlamorousToolkit
+curl -L https://github.com/feenkcom/gtoolkit/releases/latest/download/GlamorousToolkitVM-linux64-bin.zip -o GlamorousToolkitVM-linux64-bin.zip
+unzip GlamorousToolkitVM-linux64-bin.zip
+
+#get the linux ui libs
+curl -L  https://github.com/feenkcom/gtoolkit/releases/latest/download/libLinux64.zip -o libLinux64.zip
+unzip libLinux64.zip
+mv libLinux64-v*/* .
+ls -al
 #load our own code on top of pharo
-./glamoroustoolkit Pharo.image st load.st
+./glamoroustoolkit GlamorousToolkit.image st ../load.st
